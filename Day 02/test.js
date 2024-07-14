a21231ae8ef49d7621071ff0e676b42c75e85a1f
Abhishek Khatana <120416873+khatana095@users.noreply.github.com>
2024-07-14 18:07:51 +0000

    Container config done

---
+++ Day 02/test.js
@@ -0,0 +1,77 @@
+//Day 2
+
+// Activity 01 : Arithmetic opertions
+//Task 01 : Add Two No.
+let num1 = 5;
+let num2 = 10;
+let sum = num1 + num2;
+console.log(sum);
+
+//Task 02 : Sub Two No.
+let num3 = 10;
+let num4 = 20;
+let difference = num3 - num4;
+console.log(difference);
+
+//Task 03 : Multiply Two No.
+let num5 = 10;
+let num6 = 20;
+let product = num5 * num6;
+console.log(product);
+
+//Task 04 : Divide Two No.
+let num7 = 10;
+let num8 = 20;
+let quotient = num7 / num8;
+console.log(quotient);
+
+//Task 05 : Find the remainder
+let num9 = 10;
+let num10 = 20;
+let remainder = num9 % num10;
+console.log(remanider);
+
+// Activity 02 : Assigment operator
+//Task 06 : Use += operator (to add a number)
+let num11 = 100;
+num11 += 110;
+console.log(num11);
+
+//Task 07 : Use -= operator (to sub a number)
+let num12 = 100;
+num12 += 90;
+console.log(num12);
+
+// Activity 03 : Comparison operator
+// Task 08 : Compare using >and<
+let num13 = 120;
+let num14 = 130;
+console.log(num13 > num14);
+console.log(num13 < num14);
+
+// Task 09 : Compare using >= and <=
+let num15 = 120;
+let num16 = 130;
+console.log(num15 >= num16);
+console.log(num15 <= num16);
+
+// Task 10 : Compare using == and ==
+let num17 = 120;
+let num18 = 120;
+console.log(num17 == num18);
+console.log(num17 == num18);
+
+// Activity 04 : Logical operators
+// Task 11 : Use && operator
+let num19 = true
+let num20 = false
+console.log(num19 && num20);
+
+// Task 12 : Use || operator
+console.log(num19 || num20);
+
+// Task 13 : Use ! operator
+console.log(!num19);
+
+// Activity 05 : Ternary operators
+// Task 14 : Check if number is + or -
